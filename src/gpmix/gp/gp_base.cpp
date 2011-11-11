bf6c11c34115ef7598cf5c60027e6ae9071719cc
Oliver Stegle <oliver@stegle.info>
2011-11-11 01:04:38 +0100

    initial push with loads of definitions and empyt classes

---
+++ src/gpmix/gp/gp_base.cpp
@@ -0,0 +1,21 @@
+/*
+ * gp_base.cpp
+ *
+ *  Created on: Nov 11, 2011
+ *      Author: stegle
+ */
+
+#include "gp_base.h"
+
+namespace gpmix {
+
+CGPbase::CGPbase() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CGPbase::~CGPbase() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace gpmix */
