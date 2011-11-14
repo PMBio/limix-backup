8b5d0463997f04339e4eb748fd13a3c142cb2b9d
Oliver Stegle <oliver@stegle.info>
2011-11-14 22:55:50 +0100

    added code

--- src/gpmix/covar/se.h
+++ src/gpmix/covar/se.h
@@ -15,10 +15,13 @@ namespace gpmix {
 class CCovSqexpARD: public gpmix::ACovarianceFunction {
 public:
 	CCovSqexpARD(const uint_t dimensions): ACovarianceFunction(dimensions)
-	{}
+	{
+		hyperparams = 1+dimensions;
+	}
 	CCovSqexpARD(const uint_t dimensions_i0,const uint_t dimensions_i1) : ACovarianceFunction(dimensions_i0,dimensions_i1)
-	{}
-
+	{
+		hyperparams = 1+(dimensions_i1-dimensions_i0);
+	}
 	MatrixXd K(const CovarParams params, const CovarInput x1, const CovarInput x2) const;
 	VectorXd Kdiag(const CovarParams params, const CovarInput x1) const;
 	MatrixXd Kgrad_theta(const CovarParams params, const CovarInput x1, const uint_t i) const;
