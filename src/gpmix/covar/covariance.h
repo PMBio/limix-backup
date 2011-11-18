906028182861efe5119ebc30469493948cad54c6
Christoph Lippert <clippert@terek.as.localnet>
2011-11-18 19:49:39 +0100

    covarince refactoring started

--- src/gpmix/covar/covariance.h
+++ src/gpmix/covar/covariance.h
@@ -16,68 +16,102 @@ namespace gpmix {
 //define covariance function types
 
 typedef MatrixXd CovarInput;
-typedef MatrixXd CovarParams;
+typedef VectorXd CovarParams;
 
 
 class ACovarianceFunction {
 protected:
-	//number of hyperparameters
-	uint_t hyperparams;
-	//number of dimensions
-	uint_t dimensions;
-	//indices for slicing out dimensions from X
-	uint_t dimensions_i0;
-	uint_t dimensions_i1;
+	//indicator if the class is synced with the cache
+	bool insync;
 
-	//project a full set of inputs onto the targets of this class:
-	CovarInput getX(const CovarInput x) const;
-	//is a particular dimension d within the targets of this class?
-	bool dimension_is_target(const uint_t d) const;
+	//the inputs of the kernel
+	CovarInput X;
 
+	//the hyperparameters of K
+	CovarParams params;
+
+	uint_t numberParams;
 public:
 	//constructors
 #ifndef SWIG
-	ACovarianceFunction(const uint_t dimensions);
+	ACovarianceFunction(const uint_t numberParams);
 #endif
-	ACovarianceFunction(const uint_t dimensions_i0,const uint_t dimensions_i1);
 	//destructors
 	virtual ~ACovarianceFunction();
 	//getters and setters
 
-
-
-
 #ifndef SWIG
 	//remove these members due to swig wrapping confusion of overladed function
-	virtual MatrixXd K(const CovarParams params, const CovarInput x1) const;
-	virtual MatrixXd K(const CovarParams params, const CovarInput x1, const CovarInput x2) const =0;
-	virtual VectorXd Kdiag(const CovarParams params, const CovarInput x1) const;
 
-	virtual MatrixXd Kgrad_x(const CovarParams params, const CovarInput x1, const uint_t d) const;
-	virtual MatrixXd Kgrad_theta(const CovarParams params, const CovarInput x1, const uint_t i) const =0;
-	virtual MatrixXd Kgrad_x(const CovarParams params, const CovarInput x1, const CovarInput x2, const uint_t d) const=0;
-	virtual MatrixXd Kgrad_xdiag(const CovarParams params, const CovarInput x1, const uint_t d) const=0;
+	//computeK(X,X)
+	virtual MatrixXd K() const;
+
+	//compute K(Xstar,X)
+	virtual MatrixXd Kcross( const CovarInput& Xstar ) const = 0;
+	virtual VectorXd Kdiag() const;
+	virtual MatrixXd K_grad_X(const uint_t d) const;
+	virtual MatrixXd K_grad_param(const uint_t i) const = 0;
+
+	//gradient of K(Xstar,X)
+	virtual MatrixXd Kcross_grad_X(const CovarInput& Xstar, const uint_t d) const = 0;
+	virtual MatrixXd Kdiag_grad_X(const uint_t d) const = 0;
+
 	//class information
 	virtual string getName() const = 0;
+
+	//get the Vector of hyperparameters
+	inline CovarParams getParams() const {return params;}
+
+	//check if object is  insync with cache
+	inline bool isInSync() const {return insync;}
+
+	//indicate that the cache has been cleared and is synced again
+	inline void makeSync() { insync = true;}
+
+	//set the parameters to a new value.
+	void setParams(CovarParams& params);
+
+	//set X to a new value
+	inline void setX(CovarInput& X);
+
+	//get the X
+	inline CovarInput getX() const;
+
+	inline uint_t getDimX() const {return (uint_t)this->X.cols();}
 #endif
 
-
 	//getters and setters
-	uint_t getDimensions() const;
-    uint_t getDimensionsI0() const;
-    uint_t getDimensionsI1() const;
-    uint_t getHyperparams() const;
+	inline uint_t getNumberParams() const;
 
-    /* Static methods */
+
+
+    /* Static methods
     //grad checking functions
     static bool check_covariance_Kgrad_theta(const ACovarianceFunction& covar,const CovarParams params, const CovarInput x,float_t relchange=1E-5,float_t threshold=1E-2);
     static bool check_covariance_Kgrad_x(const ACovarianceFunction& covar,const CovarParams params, const CovarInput x,float_t relchange=1E-5,float_t threshold=1E-2);
     //same, however auto generating random parameters:
     static bool check_covariance_Kgrad_theta(const ACovarianceFunction& covar,const uint_t n_rows = 100,float_t relchange=1E-5,float_t threshold=1E-2);
     static bool check_covariance_Kgrad_x(const ACovarianceFunction& covar,const uint_t n_rows = 100,float_t relchange=1E-5,float_t threshold=1E-2);
+    */
 };
 
 
+	//set X to a new value
+	inline void ACovarianceFunction::setX(CovarInput& X)
+	{
+		this->X = X;
+		this->insync = false;
+	}
+
+	inline CovarInput ACovarianceFunction::getX() const
+	{
+		return this->X;
+	}
+
+	inline uint_t ACovarianceFunction::getNumberParams() const
+	{
+		return this->numberParams;
+	}
 
 //gradcheck tools for covaraince functions:
 
