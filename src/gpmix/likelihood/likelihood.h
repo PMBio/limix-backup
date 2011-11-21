4774818cfdc2a7f9afa889927a997b0cdd654d87
Christoph Lippert <clippert@terek.as.localnet>
2011-11-21 22:24:27 +0100

    minor edits
    Former-commit-id: a826bacfcd2a87f50be9ee4b1f140e51e04dc2a6

--- src/gpmix/likelihood/likelihood.h
+++ src/gpmix/likelihood/likelihood.h
@@ -14,26 +14,47 @@
 namespace gpmix {
 
 
-typedef MatrixXd LikParams;
+typedef VectorXd LikParams;
 
 class ALikelihood {
+	//indicator if the class is synced with the cache
+	bool insync;
+	LikParams params;
+	uint_t numberParams;
 public:
-	ALikelihood();
+	ALikelihood(const uint_t numberParams);
 	virtual ~ALikelihood();
-	virtual void applyToK(const LikParams& params, MatrixXd& K) const = 0;
-	virtual MatrixXd K_grad_theta(const LikParams& params, MatrixXd X, uint_t row) const = 0;
-	virtual MatrixXd K(const LikParams& params, MatrixXd& X) const = 0;
-	virtual VectorXd Kdiag(const LikParams& params, MatrixXd& X) const = 0;
+	virtual void applyToK(const MatrixXd& X, MatrixXd& K) const = 0;
+	virtual MatrixXd K_grad_params(const MatrixXd& X, const uint_t row) const = 0;
+	virtual MatrixXd K(const MatrixXd& X) const = 0;
+	virtual VectorXd Kdiag(const MatrixXd& X) const = 0;
+	//class information
+	virtual string getName() const = 0;
+
+	//get the Vector of hyperparameters
+	inline LikParams getParams() const {return params;}
+
+	//set the parameters to a new value.
+	virtual void setParams(LikParams& params);
+
+	//check if object is  insync with cache
+	inline bool isInSync() const {return insync;}
+
+	//indicate that the cache has been cleared and is synced again
+	inline void makeSync() { insync = true;}
+
 };
 
 class CLikNormalIso : public ALikelihood {
 public:
 	CLikNormalIso();
 	~CLikNormalIso();
-	void applyToK(const LikParams& params, MatrixXd& K) const;
-	MatrixXd K_grad_theta(const LikParams& params, MatrixXd X, uint_t row) const;
-	MatrixXd K(const LikParams& params, MatrixXd& X) const;
-	VectorXd Kdiag(const LikParams& params, MatrixXd& X) const;
+	void applyToK(const MatrixXd& X, MatrixXd& K) const;
+	MatrixXd K_grad_params(const MatrixXd& X, const uint_t row) const;
+	MatrixXd K(const MatrixXd& X) const;
+	VectorXd Kdiag(const MatrixXd& X) const;
+	//class information
+	string getName() const;
 };
 
 
