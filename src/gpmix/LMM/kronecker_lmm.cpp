3f8c68c5c7debe1fa1f0d0fb1436939b36c08868
Christoph Lippert <clippert@terek.as.localnet>
2012-02-16 21:41:02 +0100

    kronecker nLLeval v0.1

--- src/gpmix/LMM/kronecker_lmm.cpp
+++ src/gpmix/LMM/kronecker_lmm.cpp
@@ -45,7 +45,8 @@ namespace gpmix {
         mfloat_t ldeltaopt_glob = 0;
         MatrixXd f_tests;
         for(muint_t i = 0;i < numintervals;i++){
-            nllgrid(i, 0) = CKroneckerLMM::nLLeval(&f_tests, ldeltagrid(i), WkronDiag, WkronBlock, UX, UYU, S_C, S_R);
+        	std::cout <<"commented out the nLL\n";//TODO commented out the nLL
+            nllgrid(i, 0) = 0.0;//CKroneckerLMM::nLLeval(&f_tests, ldeltagrid(i), WkronDiag, WkronBlock, UX, UYU, S_C, S_R);
             ldeltagrid(i, 0) = ldelta;
             //std::cout<< "nnl( " << ldelta << ") = " << nllgrid(i,0) <<  "VS" << nllmin << ")\n\n";
             if(nllgrid(i, 0) < nllmin){
@@ -61,7 +62,7 @@ namespace gpmix {
         return ldeltaopt_glob;
     }
 
-    mfloat_t CKroneckerLMM::nLLeval(MatrixXd& W, MatrixXd& F_tests, mfloat_t ldelta, const std::vector<MatrixXd>& A, const std::vector<MatrixXd>& X, const MatrixXd& Y, const VectorXd& S_C, const VectorXd& S_R)
+    mfloat_t CKroneckerLMM::nLLeval(std::vector<MatrixXd>& W, std::vector<MatrixXd>& F_tests, mfloat_t ldelta, const std::vector<MatrixXd>& A, const std::vector<MatrixXd>& X, const MatrixXd& Y, const VectorXd& S_C, const VectorXd& S_R)
     {
         muint_t R = (muint_t)Y.rows();
         muint_t C = (muint_t)Y.cols();
@@ -85,18 +86,25 @@ namespace gpmix {
         	for (muint_t c=0; c<C;++c)
         	{
         		mfloat_t SSd = S_R.data()[r]*S_C.data()[c] + delta;
-        		ldet+=std::log(SSd);
+        		ldet+=log(SSd);
         		D(r,c) = 1.0/SSd;
         	}
         }
 
+        MatrixXd DY = Y.array() * D.array();
+
+        VectorXd XYA = VectorXd(nWeights);
+
         muint_t cumSumRowR = 0;
         muint_t cumSumColR = 0;
+
         MatrixXd covW = MatrixXd(nWeights,nWeights);
         for(muint_t termR = 0; termR < A.size();++termR){
         	muint_t nW_AR = A[termR].rows();
         	muint_t nW_XR = A[termR].cols();
         	muint_t rowsBlock = nW_AR * nW_XR;
+
+        	XYA.block(cumSumRowR,0,rowsBlock,1) = X[termR].transpose() * DY * A[termR].transpose();
 
         	muint_t cumSumRowC = 0;
         	muint_t cumSumColC = 0;
@@ -105,29 +113,58 @@ namespace gpmix {
             	muint_t nW_XC = A[termC].cols();
             	muint_t colsBlock = nW_AC * nW_XC;
             	MatrixXd block = MatrixXd::Zero(rowsBlock,colsBlock);
-            	if (1)//(R<C)
+            	if (R<C)
             	{
             		for(muint_t r=0; r<R; ++r)
             		{
-                		MatrixXd AD = A[termR].colwise() * D.block(r,0,1,C);
+                		MatrixXd AD = A[termR];
+                		AD.array().rowwise() *= D.row(r).array();
                 		MatrixXd AA = AD * A[termC].transpose();
                 		//sum up col matrices
                 		MatrixXd XX = X[termR].row(r).transpose() * X[termC].row(r);
-                		block += kron(AA,XX);
+                		akron(block,AA,XX,true);
             		}
             	}
             	else
             	{//sum up col matrices
             		for(muint_t c=0; c<C; ++c)
             		{
-
+            			MatrixXd XD = X[termR];
+            			XD.array().colwise() *= D.col(c).array();
+            			MatrixXd XX = XD.transpose() * X[termC];
+            			//sum up col matrices
+            			MatrixXd AA = A[termR].col(c) * A[termC].col(c).transpose();
+            			akron(block,AA,XX,true);
             		}
             	}
             	covW.block(cumSumRowR * cumSumColR, cumSumRowC * cumSumColC,rowsBlock,colsBlock) = block;
             }
         }
 
-        mfloat_t nLL = 0.0;
+        covW = covW.inverse();
+        MatrixXd W_vec = covW * XYA;
+
+        mfloat_t res = (Y.array()*DY.array()).sum();
+        res -= (W_vec.array() * XYA.array()).sum();
+
+        mfloat_t sigma = res/(mfloat_t)(R*C);
+
+        mfloat_t nLL = 0.5 * ( R * C * (L2pi + log(sigma)) + ldet);
+
+        muint_t cumSum = 0;
+        VectorXd F_vec = W_vec.array() * W_vec.array() /covW.diagonal().array() / sigma;
+        for(muint_t term = 0; term < A.size();++term)
+		{
+        	muint_t currSize = X[term].cols() * A[term].rows();
+			//W[term] = MatrixXd(X[term].cols(),A[term].rows());
+			W[term] = W_vec.block(cumSum,0,currSize,1);//
+			W[term].resize(X[term].cols(),A[term].rows());
+			//F_tests[term] = MatrixXd(X[term].cols(),A[term].rows());
+			F_tests[term] = F_vec.block(cumSum,0,currSize,1);//
+			F_tests[term].resize(X[term].cols(),A[term].rows());
+			cumSum+=currSize;
+		}
+
         return nLL;
     }
 
@@ -151,7 +188,8 @@ namespace gpmix {
         MatrixXd f_tests;
         //fit delta on null model
         ldelta0(0) = CKroneckerLMM::optdelta(Ucovs, Upheno, this->S_C, this->S_R, this->num_intervals0, this->ldeltamin0, this->ldeltamax0, WkronDiag0, WkronBlock0);
-        nLL0(0) = this->nLLeval(&f_tests, ldelta0(0), WkronDiag0, WkronBlock0, Ucovs, Upheno, this->S_C, this->S_R);
+    	std::cout <<"commented out the nLL\n";//TODO commented out the nLL
+        nLL0(0) = 0.0;//this->nLLeval(&f_tests, ldelta0(0), WkronDiag0, WkronBlock0, Ucovs, Upheno, this->S_C, this->S_R);
         for(muint_t is = 0;is < (muint_t)(((((snps.cols())))));is++){
             //1. construct foreground testing SNP transformed
             UX.block(0, covs.cols(), snps.rows(), 1) = Usnps.block(0, is, snps.rows(), 1);
@@ -165,14 +203,15 @@ namespace gpmix {
 
             //3. evaluate
             MatrixXd f_tests;
-            nLL(0, is) = CKroneckerLMM::nLLeval(&f_tests, ldelta(0, is), WkronDiag, WkronBlock, UX, Upheno, this->S_C, this->S_R);
+        	std::cout <<"commented out the nLL\n";//TODO commented out the nLL
+            nLL(0, is) = 0.0;//CKroneckerLMM::nLLeval(&f_tests, ldelta(0, is), WkronDiag, WkronBlock, UX, Upheno, this->S_C, this->S_R);
             //				gpmix::nLLeval(&f_tests, ldelta(0, is), Upheno, UX, S);
             //TODO:
             //rotate F_tests to get per pheno P-values
             //4. calc lod score
             //TODO: calculate dofs for arbitrary WkronDiag and WkronBlock, currently we expect all ones...
             mfloat_t dof = ((mfloat_t)(((((Upheno.cols()))))) / (mfloat_t)(((((WkronBlock.rows()))))));
-            this->pv(0, is) = Gamma::gammaQ(nLL0(0, 0) - nLL(0, is), (double)(((((((0.5) * dof)))))));
+            this->pv(0, is) = Gamma::gammaQ((double)(nLL0(0, 0) - nLL(0, is)), (double)(((((((0.5) * dof)))))));
     } //end for phenotypes
 }
 
