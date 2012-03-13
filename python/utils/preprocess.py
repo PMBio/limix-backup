fe53451909e5345d5355908c5071210575fdc6d2
Oliver Stegle <oliver.stegle@tuebingen.mpg.de>
2012-03-13 19:36:27 +0100

    updated python interface for verbose variables
    Former-commit-id: bdf9884cb391a9bd9a017f935bd55ed8351d352c

--- python/utils/preprocess.py
+++ python/utils/preprocess.py
@@ -2,6 +2,8 @@
 
 import scipy as SP
 import scipy.special as special
+import scipy.stats as st
+import pdb
 
 
 def scale_K(K, verbose=False):
@@ -19,8 +21,21 @@ def rankStandardizeNormal(X):
     standardize X: [samples x phenotypes]
     - each phentoype is converted to ranks and transformed back to normal using the inverse CDF
     """
-    rank = (1+SP.array(X.argsort(axis=0),dtype='float'))
-    rank /= (X.shape[0]+1)
-    #apply inverse gaussian cdf
-    RV = SP.sqrt(2) * special.erfinv(2*rank-1)
+    Is = X.argsort(axis=0)
+    RV = SP.zeros_like(X)
+    rank = SP.zeros_like(X)
+    for i in xrange(X.shape[1]):
+        x =  X[:,i]
+        if 0:
+            Is = x.argsort()
+            rank = SP.zeros_like(x)
+            rank[Is] = SP.arange(X.shape[0])
+            #add one to ensure nothing = 0
+            rank +=1
+        else:
+            rank = st.rankdata(x)
+        #devide by (N+1) which yields uniform [0,1]
+        rank /= (X.shape[0]+1)
+        #apply inverse gaussian cdf
+        RV[:,i] = SP.sqrt(2) * special.erfinv(2*rank-1)
     return RV
