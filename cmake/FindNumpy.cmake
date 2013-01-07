39630617fc0ecf86736e6ffd6a9877be6ea671e0
unknown <christoph.a.lippert@gmail.com>
2013-01-07 14:54:20 -0800

    windows bugfix of cmake scripts

--- cmake/FindNumpy.cmake
+++ cmake/FindNumpy.cmake
@@ -4,7 +4,7 @@
 #  NUMPY_FOUND, If false, do not try to use numpy headers.
 if (NOT NUMPY_INCLUDE_DIR)
     exec_program ("${PYTHON_EXECUTABLE}"
-      ARGS "-c 'import numpy; print numpy.get_include()'"
+      ARGS "-c \"import numpy; print numpy.get_include()\""
       OUTPUT_VARIABLE NUMPY_INCLUDE_DIR
       RETURN_VALUE NUMPY_NOT_FOUND)
     if (NUMPY_INCLUDE_DIR MATCHES "Traceback")
