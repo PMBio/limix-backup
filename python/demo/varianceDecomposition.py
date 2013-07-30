fixed test for variance Decomposition to be platform independent

python/demo/varianceDecomposition.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('./../../release.darwin/interfaces/python/')
+sys.path.append('./../../release.linux2/interfaces/python/')
+#sys.path.append('./../../release.darwin/interfaces/python/')
 import scipy as SP
 import scipy.stats
 import pdb
@@ -35,6 +36,7 @@ fixed1 = SP.kron(SP.array([1,0]),SP.ones((N,1)))
 fixed2 = SP.kron(SP.array([0,1]),SP.ones((N,1)))
 vc.addFixedTerm(fixed1)
 vc.addFixedTerm(fixed2)
+pdb.set_trace()
 
 # Random initialisation and Fitting
 vc.initialise()

tests/varDecomp/test_varDecomp.py
@@ -3,14 +3,20 @@ import unittest
 import scipy as SP
 import scipy.stats
 import pdb
+import os
+import sys
 import limix
+
+geno_file = './varDecomp/genotype.csv'
+pheno_file = './varDecomp/phenotype.csv'
+param_file = './varDecomp/params_true.csv'
 
 class CVarianceDecomposition_test(unittest.TestCase):
     """test class for CVarianceDecomposition"""
 
     def genGeno(self):
         self.X  = (SP.rand(self.N,self.S)<0.2)*1.
-        self.Kg = SP.dot(self.X,self.X.T)
+
     
     def genPheno(self):
         dp = SP.ones(self.P); dp[1]=-1
@@ -28,12 +34,27 @@ class CVarianceDecomposition_test(unittest.TestCase):
         self.Y=SP.stats.zscore(Y,0)
     
     def setUp(self):
-        SP.random.seed(1)
-        self.N = 200
-        self.S = 1000
-        self.P = 2
-        self.genGeno()
-        self.genPheno()
+        #check: do we have a csv File?
+        if ((not os.path.exists(geno_file)) | (not os.path.exists(pheno_file))) or 'recalc' in sys.argv:
+            SP.random.seed(1)
+            self.N = 200
+            self.S = 1000
+            self.P = 2
+            self.genGeno()
+            self.genPheno()
+            self.generate = True
+            SP.savetxt(geno_file,self.X)
+            SP.savetxt(pheno_file,self.Y)
+        else:
+            self.generate=False
+            self.X = SP.loadtxt(geno_file)
+            self.Y = SP.loadtxt(pheno_file)
+            self.N = self.X.shape[0]
+            self.S = self.X.shape[1]
+            self.P = self.Y.shape[1]
+
+        self.Kg = SP.dot(self.X,self.X.T)
+
         self.vd = limix.CVarianceDecomposition(self.Y)
         self.vd.addFixedEffTerm("specific")
         self.vd.addTerm("Dense",self.Kg)
@@ -46,7 +67,11 @@ class CVarianceDecomposition_test(unittest.TestCase):
         """ optimization test """
         self.vd.trainGP()
         params = self.vd.getOptimum()['scales'][:,0]
-        params_true = SP.array([-9.44207121e-01,+9.37361082e-01,5.35657525e-01,1.42134130e-08,-1.65923192e-08,3.09197580e-09])
+        if self.generate:
+            SP.savetxt(param_file,params)
+            params_true = SP.zeros_like(params)
+        else:
+            params_true = SP.loadtxt(param_file)
         RV = (params-params_true).max()<1e-6
         self.assertTrue(RV)
 