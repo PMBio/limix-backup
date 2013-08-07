9cbd8f88ca697fc493185a94fd2c296abd23e302
paolo.casale <casale@ebi.ac.uk>
2013-08-07 22:06:06 +0200

    minor changes

--- python/demo/gpKronSum.py
+++ python/demo/gpKronSum.py
@@ -34,15 +34,25 @@ covarr1 = limix.CFixedCF(K)
 covarr2 = limix.CFixedCF(SP.eye(N))
 covarr1.setParamMask(SP.zeros(1))
 covarr2.setParamMask(SP.zeros(1))
-# CKroneckerMean
-F=SP.ones((N,1))
-A=SP.eye(P)
-W=SP.zeros((1,P))
-mean = limix.CLinearMean()
-mean = limix.CKroneckerMean()
-mean.setA(A)
-mean.setFixedEffects(F)
-mean.setParams(W)
+# CLinearMean
+F1=SP.ones((N,1))
+W1=SP.zeros((1,P))
+A1=SP.eye(P)
+mean1 = limix.CKroneckerMean()
+mean1.setA(A1)
+mean1.setFixedEffects(F1)
+mean1.setParams(W1)
+F2=SP.randn(N,1)
+W2=SP.zeros((1,1))
+A2=SP.ones((1,P))
+mean2 = limix.CKroneckerMean()
+mean2.setA(A2)
+mean2.setFixedEffects(F2)
+mean2.setParams(W2)
+mean = limix.CSumLinear()
+mean.appendTerm(mean1)
+mean.appendTerm(mean2)
+mean.aGetParams()
 # GPkronSum
 gpKS = limix.CGPkronSum(Y,covarr1,covarc1,covarr2,covarc2,limix.CLikNormalNULL(),mean)
 # initalization
@@ -51,7 +61,7 @@ params['covarc1']=SP.randn(covarc1.getNumberParams())
 params['covarc2']=SP.randn(covarc2.getNumberParams())
 params['covarr1']=SP.ones(1)
 params['covarr2']=SP.ones(1)
-params['dataTerm']=SP.randn(1,P)
+params['dataTerm']=SP.randn(P+1,1)
 gpKS.setParams(params)
 # Paramask
 paramMask = limix.CGPHyperParams()
@@ -70,6 +80,7 @@ paramsOpt = gpKS.getParams()
 print ""
 print "optimised parameters"
 print SP.concatenate((paramsOpt['covarc1'],paramsOpt['covarr1'],paramsOpt['covarc2'],paramsOpt['covarr2']))
+print paramsOpt['dataTerm']
 print ""
 
 print "GP BASE"
@@ -81,13 +92,14 @@ covar.addCovariance(limix.CKroneckerCF(covarc1,covarr1))
 covar.addCovariance(limix.CKroneckerCF(covarc2,covarr2))
 # Mean
 mean1 = limix.CLinearMean()
-mean1.setFixedEffects(SP.kron(A,F))
+F = SP.concatenate((SP.kron(A1.T,F1),SP.kron(A2.T,F2)),1)
+mean1.setFixedEffects(F)
 gp = limix.CGPbase(covar,limix.CLikNormalNULL(),mean1)
 gp.setY(Y1)
 # initialisation
 Params = limix.CGPHyperParams()
 Params['covar']=SP.concatenate((params['covarc1'],params['covarr1'],params['covarc2'],params['covarr2']))
-Params['dataTerm']=params['dataTerm'].T
+Params['dataTerm']=params['dataTerm']
 gp.setParams(Params)
 # optimisation
 gpopt = limix.CGPopt(gp)
@@ -96,9 +108,11 @@ gpopt.opt()
 elapsed_time = time.time() - start_time
 print "time elapsed(s): "+str(elapsed_time)
 
+paramsOpt1 = gp.getParams()
 print ""
 print "optimised parameters"
-print gp.getParams()['covar']
+print paramsOpt1['covar']
+print paramsOpt1['dataTerm']
 print ""
 
 
