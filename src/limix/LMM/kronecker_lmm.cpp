750523a9c971962d00b739262037039754cdae04
unknown <christoph.a.lippert@gmail.com>
2013-09-04 23:47:33 +0100

    df

--- src/limix/LMM/kronecker_lmm.cpp
+++ src/limix/LMM/kronecker_lmm.cpp
@@ -26,99 +26,113 @@ namespace limix {
 
         if(!(this->UK_cached)){
             //decomposition of K
-			Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver1c(K1c);
-            this->U1c = eigensolver1c.eigenvectors();
-            this->S1c = eigensolver1c.eigenvalues();
-			if (this->S1c(0)<=1e-12){
-				throw new CGPMixException("The column covariance of the first covariance term has to be full rank, but is not.");
+			Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver2c(K2c);
+            this->U2c = eigensolver2c.eigenvectors();
+            this->S2c = eigensolver2c.eigenvalues();
+			if (this->S2c(0)<=1e-12){
+				throw new CGPMixException("The column covariance of the second covariance term has to be full rank, but is not.");
 			}
 
-			Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver1r(K1r);
-            this->U1r = eigensolver1r.eigenvectors();
-            this->S1r = eigensolver1r.eigenvalues();
-			if (this->S1r(0)<=1e-12){
-				throw new CGPMixException("The row covariance of the first covariance term has to be full rank, but is not.");
+			Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver2r(K2r);
+            this->U2r = eigensolver2r.eigenvectors();
+            this->S2r = eigensolver2r.eigenvalues();
+			if (this->S2r(0)<=1e-12){
+				throw new CGPMixException("The row covariance of the second covariance term has to be full rank, but is not.");
 			}
             
-			this->S1U1K2r = U1r.transpose() * this->K2r * U1r;
+			this->S2U2K1r = U2r.transpose() * this->K1r * U2r;
 			for (size_t r1=0;r1<this->pheno.rows();++r1)
 			{
 				for (size_t r2=0;r2<this->pheno.rows();++r2)
 				{
-					this->S1U1K2r(r1,r2)/=sqrt(S1r(r1));
-					this->S1U1K2r(r1,r2)/=sqrt(S1r(r2));
+					this->S2U2K1r(r1,r2)/=sqrt(S2r(r1));
+					this->S2U2K1r(r1,r2)/=sqrt(S2r(r2));
 				}
 			}
 
-			this->S1U1K2c = U1c.transpose() * this->K2c * U1c;
+			this->S2U2K1c = U2c.transpose() * this->K1c * U2c;
 			for (size_t c1=0;c1<this->pheno.cols();++c1)
 			{
 				for (size_t c2=0;c2<this->pheno.cols();++c2)
 				{
-					this->S1U1K2r(c1,c2)/=sqrt(S1r(c1));
-					this->S1U1K2r(c1,c2)/=sqrt(S1r(c2));
+					this->S2U2K1r(c1,c2)/=sqrt(S2r(c1));
+					this->S2U2K1r(c1,c2)/=sqrt(S2r(c2));
 				}
 			}
 
-			Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver2c(S1U1K2c);
-            this->U2c = eigensolver2c.eigenvectors();
-            this->S2c = eigensolver2c.eigenvalues();
+			Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver1c(S2U2K1c);
+            this->U1c = eigensolver1c.eigenvectors();
+            this->S1c = eigensolver1c.eigenvalues();
 
-			Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver2r(S1U1K2r);
-            this->U2r = eigensolver2r.eigenvectors();
-            this->S2r = eigensolver2r.eigenvalues();
+			Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver1r(S2U2K1r);
+            this->U1r = eigensolver1r.eigenvectors();
+            this->S1r = eigensolver1r.eigenvalues();
             
 			this->UK_cached = true;
         }
         
 		if(!Usnps_cached){
-            Usnps.noalias() = U1r.transpose() * snps;
+            Usnps.noalias() = U2r.transpose() * snps;
 			for (size_t r=0;r<this->pheno.rows();++r)
 			{
 				for (size_t s=0;s<this->snps.cols();++s)
 				{
-					this->Usnps(r,s)/=sqrt(S1r(s));
+					this->Usnps(r,s)/=sqrt(S2r(s));
 				}
 			}
-			Usnps.noalias()= U2r.transpose() * Usnps;
+			Usnps.noalias()= U1r.transpose() * Usnps;
             Usnps_cached = true;
         }
         if(!Upheno_cached){
-            Upheno.noalias() = U1r.transpose() * pheno * U1c;
+            Upheno.noalias() = U2r.transpose() * pheno * U2c;
 			for (size_t r=0;r<this->pheno.rows();++r)
 			{
 				for (size_t c=0;c<this->pheno.cols();++c)
 				{
-					this->Upheno(r,c)/=sqrt(S1r(r));
-					this->Upheno(r,c)/=sqrt(S1c(c));
+					this->Upheno(r,c)/=sqrt(S2r(r));
+					this->Upheno(r,c)/=sqrt(S2c(c));
 				}
 			}
-			Usnps.noalias()= U2r.transpose() * Usnps;
+			Upheno.noalias()= U1r.transpose() * Upheno *U1c;
 			Upheno_cached = true;
         }
         if(!Ucovs_cached){
-            Ucovs.noalias() = U1r.transpose() * covs;
+            Ucovs.noalias() = U2r.transpose() * covs;
 			for (size_t r=0;r<this->pheno.rows();++r)
 			{
 				for (size_t d=0;d<this->covs.cols();++d)
 				{
-					this->Ucovs(r,d)/=sqrt(S1r(r));
+					this->Ucovs(r,d)/=sqrt(S2r(r));
 				}
 			}
-
+			Ucovs.noalias()= U1r.transpose() * Ucovs;
             Ucovs_cached = true;
         }
 		//need column design matrix
-		if(!Ucoldesign_cached){
-            Ucoldesign.noalias() = coldesign * U1c;
-            for (size_t r=0;r<this->pheno.rows();++r)
+		if(!Ucoldesign_cached){//for covariates
+            Ucoldesign.noalias() = coldesign * U2c;
+            for (size_t r=0;r<this->Ucoldesign.rows();++r)
 			{
-				for (size_t d=0;d<this->covs.cols();++d)
+				for (size_t d=0;d<this->Ucoldesign.cols();++d)
 				{
-					this->S1U1K2r(r,d)/=sqrt(S1r(r));
+					this->Ucoldesign(r,d)/=sqrt(S2c(d));
 				}
 			}
+			Ucoldesign.noalias() = Ucoldesign * U1c;
             Ucoldesign_cached = true;
+        }
+		//need column design matrix
+		if(!Usnpcoldesign_cached){//design for SNPs
+            Usnpcoldesign.noalias() = snpcoldesign * U2c;
+            for (size_t r=0;r<this->Usnpcoldesign.rows();++r)//should be 1
+			{
+				for (size_t d=0;d<this->Usnpcoldesign.cols();++d)
+				{
+					this->Usnpcoldesign(r,d)/=sqrt(S2c(d));
+				}
+			}
+			Usnpcoldesign.noalias() = Usnpcoldesign * U1c;
+            Usnpcoldesign_cached = true;
         }
 	}
 
