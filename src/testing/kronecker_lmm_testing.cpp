f1ff14c0751482c6e317672c1a33c73250256e6e
unknown <christoph.a.lippert@gmail.com>
2013-09-17 22:01:54 -0700

    updates
    Former-commit-id: 5c0334b156025c7a870a4b71214d8b287b09fa8c

--- src/testing/kronecker_lmm_testing.cpp
+++ src/testing/kronecker_lmm_testing.cpp
@@ -30,10 +30,11 @@ int main() {
 		muint_t Wr=1;
 		muint_t Wr_covar = 2;
 		muint_t Wc_covar = 1;
-		muint_t P=2;
+		muint_t P=50;
 		muint_t N=100;
 		muint_t Wc=P;
-		muint_t Nsnp0=50;
+		muint_t Nsnp0=5000;
+		muint_t NsnpK=500;
 		mfloat_t eps1 = 1.0;
 		mfloat_t eps2 = 1.0;
 #else
@@ -48,31 +49,33 @@ int main() {
 		mfloat_t eps2 = 2.0;
 #endif
 		//1. "simulation"
-		MatrixXd Xr1=randn(N,2*N);
-		Xr1/=limix::sqrt((mfloat_t)(2.0*N));
-		MatrixXd Kr1=Xr1*Xr1.transpose()+MatrixXd::Identity(N,N);
+		MatrixXd Xr1=BaldingNichols((muint_t)N,NsnpK, 0.1, 0.005, true);
+		Xr1/=limix::sqrt((mfloat_t)(NsnpK));
+		MatrixXd Kr1=Xr1*Xr1.transpose();//+MatrixXd::Identity(N,N);
 		if (Kr1.hasNaN()){
 			std::cout << Kr1;
 		}
 		MatrixXd Xc1=randn(2*P,P);
 		Xc1/=limix::sqrt((mfloat_t)(2.0*P));
-		MatrixXd Kc1=Xc1.transpose()*Xc1+MatrixXd::Identity(P,P);
+		MatrixXd Kc1=Xc1.transpose()*Xc1;//+MatrixXd::Identity(P,P);
 		if (Kc1.hasNaN()){
 			std::cout << Kc1;
 		}
-		MatrixXd Xr2 = randn(N,2*N);
+		MatrixXd Xr2 = BaldingNichols((muint_t)N,2*N, 0.1, 0.005, true);
 		Xr2/=limix::sqrt((mfloat_t)(2.0*N));
-		MatrixXd Kr2 = Xr2*Xr2.transpose()+MatrixXd::Identity(N,N);
+		MatrixXd Kr2 = Xr2*Xr2.transpose();//+MatrixXd::Identity(N,N);
 		if (Kr2.hasNaN()){
 			std::cout << Kr2;
 		}
 		MatrixXd Xc2=randn(2*P,P);
 		Xc2/=limix::sqrt((mfloat_t)(2.0*P));
-		MatrixXd Kc2 = Xc2.transpose()*Xc2+MatrixXd::Identity(P,P);
+		MatrixXd Kc2 = Xc2.transpose()*Xc2;//+MatrixXd::Identity(P,P);
 		if (Kc2.hasNaN()){
 			std::cout << Kc2;
 		}
-		MatrixXd X = randn((muint_t)N,(muint_t)Wr);
+
+		
+		MatrixXd X = BaldingNichols((muint_t)N,Wr, 0.1, 0.005, true);
 		MatrixXd Xcovar = randn((muint_t)N,(muint_t)Wr_covar);
 		//y ~ w*X
 		MatrixXd w = randn((muint_t)Wr,(muint_t)Wc);
@@ -80,13 +83,13 @@ int main() {
 
 		MatrixXd A = MatrixXd::Identity((muint_t)Wc,(muint_t)P);
 		MatrixXd A_covar = MatrixXd::Ones((muint_t)Wc_covar,(muint_t)P);
-		MatrixXd noise1 = Xr1*1.0*eps1*randn((muint_t)2*N,(muint_t)2*P)*Xc1;
+		MatrixXd noise1 = Xr1*1.0*eps1*randn((muint_t)NsnpK,(muint_t)2*P)*Xc1;
 		MatrixXd noise2 = Xr2*1.0*eps2*randn((muint_t)2*N,(muint_t)2*P)*Xc2;
 		MatrixXd Y = X*w*A + Xcovar*wcovar*A_covar + noise1 + noise2;
 		//SNPS: all random except for one true causal guy
 
-		MatrixXd S = MatrixXd::Zero((muint_t)N,(Nsnp0+1)*Wr);
-		S.block(0,0,N,Nsnp0) = randn((muint_t)N,Nsnp0*Wr);
+		MatrixXd S = BaldingNichols((muint_t)N,(Nsnp0+1)*Wr, 0.1, 0.005, true);
+		//S.block(0,0,N,Nsnp0) = randn((muint_t)N,Nsnp0*Wr);
 		S.block(0,Nsnp0*Wr,N,Wr) = X;
 		//std::cout<<"SNPS: "<< S<<std::endl;
 		//2. construction of GP object
@@ -110,7 +113,10 @@ int main() {
 		lmm.process();
 		MatrixXd pv = MatrixXd();
 		lmm.agetPv(&pv);
-		std::cout << "pv:\n" << pv << "\n";
+		mfloat_t pvmean = pv.mean();
+		
+		//std::cout << "pv:\n" << pv << "\n";
+		std::cout << "mean(pv)="<<pvmean<<"  max(pv)="<< pv.maxCoeff() << "  min(pv)="<<pv.minCoeff()<<"\n";
 		std::cout << "done.\n" ;
 		
 		}
