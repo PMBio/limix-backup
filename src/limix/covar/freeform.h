4969d3200e7e695edacd1d9c5371e0ef58ac00a6
paolo.casale <casale@ebi.ac.uk>
2013-10-03 13:20:55 +0100

    added features to CFixedCF for making predictions

--- src/limix/covar/freeform.h
+++ src/limix/covar/freeform.h
@@ -105,33 +105,50 @@ public:
 typedef sptr<CRankOneCF> PRankOneCF;
 
 
+//rename argout operators for swig interface
+#if (defined(SWIG) && !defined(SWIG_FILE_WITH_INIT))
+//rename functions and select the visible component for SWIG
+%rename(getK0) CFixedCF::agetK0;
+%rename(getK0cross) CFixedCF::agetK0cross;
+%rename(getK0cross_diag) CFixedCF::agetK0cross_diag;
+//%sptr(gpmix::CFixedCF)
+#endif
+
 
 class CFixedCF: public ACovarianceFunction {
 
 protected:
 	muint_t numberGroups;
     MatrixXd K0;
+	MatrixXd K0cross;
+	VectorXd K0cross_diag;
 public:
 
     CFixedCF(const MatrixXd& K0);
     ~CFixedCF();
 
-	//Block X functions: X is fixed and set in the constructor
-	virtual void setX(const CovarInput& X) throw (CGPMixException) {};
-	virtual void setXcol(const CovarInput& X, muint_t col) throw (CGPMixException) {};
-	virtual void aKcross_diag(VectorXd* out, const CovarInput& Xstar) const throw(CGPMixException);
-	virtual void aKcross_grad_X(MatrixXd* out,const CovarInput& Xstar, const muint_t d) const throw(CGPMixException) {};
-	virtual void aKdiag_grad_X(VectorXd* out,const muint_t d) const throw(CGPMixException) {};
-
     virtual void agetScales(CovarParams* out);
     virtual void setParamsCovariance(const MatrixXd& K0) throw(CGPMixException);
 
-    //Covariance pure functions
-	//pure functions that need to be implemented
+	//overloaded pure virtual functions:
 	virtual void aKcross(MatrixXd* out, const CovarInput& Xstar ) const throw(CGPMixException);
+	virtual void aKcross_diag(VectorXd* out, const CovarInput& Xstar) const throw(CGPMixException);
 	virtual void aKgrad_param(MatrixXd* out,const muint_t i) const throw(CGPMixException);
-    virtual void aKhess_param(MatrixXd* out,const muint_t i,const muint_t j) const throw(CGPMixException);
+    virtual void aKhess_param(MatrixXd* out, const muint_t i, const muint_t j) const throw(CGPMixException);
+	virtual void aKcross_grad_X(MatrixXd* out,const CovarInput& Xstar, const muint_t d) const throw(CGPMixException);
+	virtual void aKdiag_grad_X(VectorXd* out,const muint_t d) const throw(CGPMixException);
+	//other overloads
+	virtual void aK(MatrixXd* out) const throw (CGPMixException);
+
     virtual void agetParamMask0(CovarParams* out) const;
+
+	//setter and getters
+	void setK0(const MatrixXd& K0);
+	void setK0cross(const MatrixXd& Kcross);
+	void agetK0(MatrixXd* out) const;
+	void agetK0cross(MatrixXd* out) const;
+	void setK0cross_diag(const VectorXd& Kcross_diag);
+	void agetK0cross_diag(VectorXd* out) const;
 
     //class information
     inline std::string getName() const {return "CFixedCF";};
