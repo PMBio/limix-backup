30b2bc9edcdf3672bc7746ff5dc0ec27a7e2722c
Oliver Stegle <oliver.stegle@ebi.ac.uk>
2014-04-23 22:55:07 +0100

    cleaned archive

--- src/archive/vcf/smithwaterman/Repeats.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <map>
-
-using namespace std;
-
-map<string, int> repeatCounts(long int pos, const string& seq, int maxsize);
-bool isRepeatUnit(const string& seq, const string& unit);
