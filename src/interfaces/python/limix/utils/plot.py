a5514d279ac3dc93f23a66583501d361bb8a52ac
Oliver Stegle <oliver.stegle@ebi.ac.uk>
2014-07-21 18:55:42 +0100

    update

--- src/interfaces/python/limix/utils/plot.py
+++ src/interfaces/python/limix/utils/plot.py
@@ -1,7 +1,7 @@
 import sys
-import scipy as SP
+import scipy as sp 
 import pdb
-import pylab as PL
+import pylab as pl 
 import matplotlib.pylab as plt
 import scipy.stats as st
 import copy
@@ -9,13 +9,12 @@ import os
 import cPickle
 import glob
 
-def plot_manhattan(plt,posCum,pv,chromBounds,
+def plot_manhattan(posCum,pv,chromBounds,
 					thr=None,qv=None,lim=None,xticklabels=True,
-					alphaNS=0.1,alphaS=0.5,colorNS='DarkBlue',colorS='Orange'):
+					alphaNS=0.1,alphaS=0.5,colorNS='DarkBlue',colorS='Orange',plt=None):
 	"""
 	This script makes a manhattan plot
 	-------------------------------------------
-	plt				matplotlib.axes.AxesSubplot
 	posCum			cumulative position
 	pv				pvalues
 	chromBounds		chrom boundaries
@@ -29,41 +28,44 @@ def plot_manhattan(plt,posCum,pv,chromBounds,
 	xticklabels		if true, xtick labels are printed
 	alphaNS			transparency of non-significant SNPs
 	alphaS			transparency of significant SNPs
+	plt				matplotlib.axes.AxesSubplot, the target handle for this figure (otherwise current axes)
 	"""
+	if plt is None:
+		plt = pl.gca()
 
 	if thr==None:
 		thr = 0.01/float(posCum.shape[0])
 
 	if lim==None:
-		lim=-1.2*SP.log10(SP.minimum(pv.min(),thr))
+		lim=-1.2*sp.log10(sp.minimum(pv.min(),thr))
 
-	chromBounds = SP.concatenate([chromBounds,SP.array([posCum.max()])])
+	chromBounds = sp.concatenate([chromBounds,sp.array([posCum.max()])])
 
 	n_chroms = chromBounds.shape[0]
 	for chrom_i in range(0,n_chroms-1,2):
-		PL.fill_between(posCum,0,lim,where=(posCum>chromBounds[chrom_i]) & (posCum<chromBounds[chrom_i+1]),facecolor='LightGray',linewidth=0,alpha=0.5)
+		pl.fill_between(posCum,0,lim,where=(posCum>chromBounds[chrom_i]) & (posCum<chromBounds[chrom_i+1]),facecolor='LightGray',linewidth=0,alpha=0.5)
 
 	if qv==None:
 		Isign = pv<thr
 	else:
 		Isign = pv<thr
 
-	PL.plot(posCum[~Isign],-SP.log10(pv[~Isign]),'.',color=colorNS,ms=5,alpha=alphaNS)
-	PL.plot(posCum[Isign], -SP.log10(pv[Isign]), '.',color=colorS,ms=5,alpha=alphaS)
+	pl.plot(posCum[~Isign],-sp.log10(pv[~Isign]),'.',color=colorNS,ms=5,alpha=alphaNS)
+	pl.plot(posCum[Isign], -sp.log10(pv[Isign]), '.',color=colorS,ms=5,alpha=alphaS)
 
-	PL.plot([0,posCum.max()],[-SP.log10(thr),-SP.log10(thr)],'--',color='Gray')
+	pl.plot([0,posCum.max()],[-sp.log10(thr),-sp.log10(thr)],'--',color='Gray')
 
-	PL.ylim(0,lim)
+	pl.ylim(0,lim)
 
-	PL.ylabel('-log$_{10}$pv')
-	PL.xlim(0,posCum.max())
-	xticks = SP.array([chromBounds[i:i+2].mean() for i in range(chromBounds.shape[0]-1)])
+	pl.ylabel('-log$_{10}$pv')
+	pl.xlim(0,posCum.max())
+	xticks = sp.array([chromBounds[i:i+2].mean() for i in range(chromBounds.shape[0]-1)])
 	plt.set_xticks(xticks)
-	PL.xticks(fontsize=6)
+	pl.xticks(fontsize=6)
 
 	if xticklabels:
-		plt.set_xticklabels(SP.arange(1,n_chroms+1))
-		PL.xlabel('genetic position')
+		plt.set_xticklabels(sp.arange(1,n_chroms+1))
+		pl.xlabel('genetic position')
 	else:
 		plt.set_xticklabels([])
 
@@ -73,65 +75,62 @@ def plot_manhattan(plt,posCum,pv,chromBounds,
 	plt.yaxis.set_ticks_position('left')
 
 
-def qqplot_bar(M=1000000, alphaLevel = 0.05, distr = 'log10'):
-    #assumes 'log10'   
-    import scipy as SP
-    import pylab as PL
-    import scipy.stats as ST
-    #pdb.set_trace()
-    mRange=10**(SP.arange(SP.log10(0.5),SP.log10(M-0.5)+0.1,0.1));#should be exp or 10**?
-    numPts=len(mRange);
-    betaalphaLevel=SP.zeros(numPts);#down in the plot
-    betaOneMinusalphaLevel=SP.zeros(numPts);#up in the plot
-    betaInvHalf=SP.zeros(numPts);
-    for n in xrange(numPts):
-        m=mRange[n]; #numPLessThanThresh=m;
-        betaInvHalf[n]=ST.beta.ppf(0.5,m,M-m);
-        betaalphaLevel[n]=ST.beta.ppf(alphaLevel,m,M-m);
-        betaOneMinusalphaLevel[n]=ST.beta.ppf(1-alphaLevel,m,M-m);
-        pass
-    betaDown=betaInvHalf-betaalphaLevel;
-    betaUp=betaOneMinusalphaLevel-betaInvHalf;
+def _qqplot_bar(M=1000000, alphaLevel = 0.05, distr = 'log10'):
+	"""calculate theoretical expectations for qqplot"""
+	mRange=10**(sp.arange(sp.log10(0.5),sp.log10(M-0.5)+0.1,0.1));#should be exp or 10**?
+	numPts=len(mRange);
+	betaalphaLevel=sp.zeros(numPts);#down in the plot
+	betaOneMinusalphaLevel=sp.zeros(numPts);#up in the plot
+	betaInvHalf=sp.zeros(numPts);
+	for n in xrange(numPts):
+	   m=mRange[n]; #numPLessThanThresh=m;
+	   betaInvHalf[n]=st.beta.ppf(0.5,m,M-m);
+	   betaalphaLevel[n]=st.beta.ppf(alphaLevel,m,M-m);
+	   betaOneMinusalphaLevel[n]=st.beta.ppf(1-alphaLevel,m,M-m);
+	betaDown=betaInvHalf-betaalphaLevel;
+	betaUp=betaOneMinusalphaLevel-betaInvHalf;
 
-    theoreticalPvals=mRange/M;
-    return betaUp, betaDown, theoreticalPvals
+	theoreticalPvals=mRange/M;
+	return betaUp, betaDown, theoreticalPvals
     
 
 def qqplot(pv, distr = 'log10', alphaLevel = 0.05):
 	"""
 	This script makes a QQ plot
 	-------------------------------------------
-	pv				pvalues
+	pv				pvalues (numpy array)
 	distr           scale of the distribution (log10 or chi2)
 	alphaLevel      significance bounds
 	"""
+	shape_ok = (len(pv.shape)==1) or ((len(pv.shape)==2) and pv.shape[1]==1)
+	assert shape_ok, 'qqplot requires a 1D array of p-values'
+
 	tests = pv.shape[0]
 	pnull = (0.5 + sp.arange(tests))/tests
 	# pnull = np.sort(np.random.uniform(size = tests))    
+	Ipv = sp.argsort(pv)
 
 	if distr == 'chi2':    
 	    qnull = sp.stats.chi2.isf(pnull, 1)   
-	    qemp = (sp.stats.chi2.isf(sp.sort(pval),1))
+	    qemp = (sp.stats.chi2.isf(pv[Ipv],1))
 	    xl = 'LOD scores'
 	    yl = '$\chi^2$ quantiles'
 
 	if distr == 'log10':
 	    qnull = -sp.log10(pnull)
-	    qemp = -sp.log10(sp.sort(pval))
+	    qemp = -sp.log10(pv[Ipv])
 	    
 	    xl = '-log10(P) observed'
 	    yl = '-log10(P) expected'
 
-	plt.figure()
 	plt.plot(qnull, qemp, '.')
-	#plt.plot([0,qemp.max()], [0,qemp.max()],'r')
+	#plt.plot([0,qemp.m0x()], [0,qemp.max()],'r')
 	plt.plot([0,qnull.max()], [0,qnull.max()],'r')
 	plt.ylabel(xl)
 	plt.xlabel(yl)
-	plt.title('Genomic control: %.3f' % estimate_lambda(pval.flatten()))
 	if alphaLevel is not None:
 	    if distr == 'log10':
-	        betaUp, betaDown, theoreticalPvals = qqplot_bar(M=tests,alphaLevel=alphaLevel,distr=distr)
+	        betaUp, betaDown, theoreticalPvals = _qqplot_bar(M=tests,alphaLevel=alphaLevel,distr=distr)
 	        lower = -sp.log10(theoreticalPvals-betaDown)
 	        upper = -sp.log10(theoreticalPvals+betaUp)
 	        plt.fill_between(-sp.log10(theoreticalPvals),lower,upper,color='grey',alpha=0.5)
