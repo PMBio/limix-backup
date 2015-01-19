ed96063e4b33b7daaa217d4ad111bd6e841f54f3
paolo.casale <casale@ebi.ac.uk>
2015-01-19 11:42:36 +0000

    mean update

--- limix/core/gp/__init__.py
+++ limix/core/gp/__init__.py
@@ -1,3 +1,4 @@
 import sys
 sys.path.append('./..')
 from gp2kronSum import gp2kronSum
+from gp2kronSum_reml import gp2kronSum_reml
--- limix/core/mean/mean.py
+++ limix/core/mean/mean.py
@@ -1,27 +1,69 @@
+import sys
+sys.path.insert(0,'./..')
+from cobj import * 
 import scipy as SP
+import copy
 import pdb
 
-class mean():
+class mean(cObject):
 
 	def __init__(self,Y):
 		""" init data term """
-		self.setY(Y)
+		self.Y = Y
+		self._init()
 		self.clearFixedEffect()
-		self.setRowRotation()
-		self.setColRotation()
+
+    #########################################
+    # Properties 
+    #########################################
+
+    @property
+    def A():
+        return self._A
+
+    @property
+    def B():
+        return self._B
+
+    @property
+    def F():
+        return self._F
+
+    @property
+    def Y():
+        return self._Y
+
+    @property
+    def Lr():
+        return self._Lr
+
+    @property
+    def Lc():
+        return self._Lc
+
+    @property
+    def d():
+        return self._d
+
+    @property
+    def D():
+        return SP.reshape(self.d,(self.N,self.P), order='F')
+
+    #########################################
+    # Setters 
+    #########################################
 
 	def clearFixedEffect(self):
 		""" erase all fixed effects """
-		self.A = []
-		self.F = []
-		self.B = [] 
-		self.Ar = []
-		self.Fr = []
+		self._A = []
+		self._F = []
+		self._B = [] 
 		self.n_terms = 0
+        self.n_fixed_effs = 0
 		self.indicator = {'term':SP.array([]),
 							'row':SP.array([]),
 							'col':SP.array([])}
-		self.designs_have_changed=True
+        self.clear_cache('Fstar','Astar','Xhat')
 
 	def addFixedEffect(self,F=None,A=None):
 		"""
@@ -33,27 +75,113 @@ class mean():
 		if A is None:	A = SP.eye(P)
 		assert F.shape[0]==self.N, "F dimension mismatch"
 		assert A.shape[1]==self.P, "A dimension mismatch"
-		self.F.append(F); self.Fr.append(SP.zeros_like(F))
-		self.A.append(A); self.Ar.append(SP.zeros_like(A))
+		self.F.append(F)
+		self.A.append(A)
 		# build B matrix and indicator
 		self.B.append(SP.zeros((F.shape[1],A.shape[0])))
 		self._update_indicator(F.shape[1],A.shape[0])
 		self.n_terms+=1
-		self.designs_have_changed=True
+        self.n_fixed_effs+=F.shape[1]*A.shape[0]
+        self.clear_cache('Fstar','Astar','Xhat')
+
+    @Y.setter
+	def Y(self,value):
+		""" set phenotype """
+		self._N,self.P = Y.shape
+		self._Y = Y
+        self.clear_cache('Ystar1','Ystar','Yhat')
+
+    @Lr.setter
+	def Lr(self,value):
+		""" set row rotation """
+	    assert value.shape[0]==self._N, 'dimension mismatch'
+		assert value.shape[1]==self._N, 'dimension mismatch'
+		self._Lr = value
+        self.clear_cache('Fstar','Ystar1','Ystar','Yhat','Xhat')
+
+    @Lc.setter
+	def Lc(self,value):
+		""" set col rotation """
+        assert value.shape[0]==self._P, 'Lc dimension mismatch'
+        assert value.shape[1]==self._P, 'Lc dimension mismatch'
+		self._Lc = value
+        self.clear_cache('Astar','Ystar','Yhat','Xhat')
+
+    @d.setter
+    def d(self,value):
+        """ set anisotropic scaling """
+        assert value.shape[0]==self._P*self._N, 'd dimension mismatch'
+        self._d = value
+        self.clear_cache('Yhat','Xhat')
+
+    #########################################
+    # Getters (caching)
+    #########################################
+
+    @cached
+    def Astar(self):
+        RV = []
+        for term_i in range(self.n_terms):
+            RV.append(SP.dot(self.getA(term_i),self.Lc.T))
+        return RV 
+
+    @cached
+    def Fstar(self):
+        RV = []
+        for term_i in range(self.n_terms):
+            RV.append(SP.dot(self.Lr,self.getF(term_i)))
+        return RV 
+
+    @cached
+    def Ystar1(self):
+        return SP.dot(self.Lr,self.Y)
+
+    @cached
+    def Ystar(self):
+        return SP.dot(self.Ystar1(),self.Lc.T)
+
+    @cached
+    def getYhat(self):
+        return self.D*self.getYstar()
+
+    @cached
+    def getXhat(self):
+        RV = SP.zeros((N*P,self.n_fixed_effs))
+        ip = 0
+        for i in range(self.n_terms):
+            Ki = self.A[i].shape[0]*self.F[i].shape[1]
+            self.Xhat[:,ip:ip+Ki] = self.d[:,SP.newaxis]*SP.kron(self.Fstar[i],self.Astar[i].T)
+            ip += Ki
+        return RV 
+
+    ###############################################
+    # Other getters with no caching, should not they have caching somehow?
+    ###############################################
 
 	def predict(self):
 		""" predict the value of the fixed effect """
-		self._rotate()
 		RV = SP.zeros((self.N,self.P))
 		for term_i in range(self.n_terms):
-			RV+=SP.dot(self.Fr[term_i],SP.dot(self.B[term_i],self.Ar[term_i]))
+			RV+=SP.dot(self.Fstar()[term_i],SP.dot(self.getB()[term_i],self.Astar()[term_i]))
 		return RV
 
 	def evaluate(self):
 		""" predict the value of """
 		RV  = -self.predict()
-		RV += self.Yr
+		RV += self.getYstar()
 		return RV
+
+	def getGradient(self,j):
+		""" get rotated gradient for fixed effect i """
+		i = int(self.indicator['term'][j])
+		r = int(self.indicator['row'][j])
+		c = int(self.indicator['col'][j])
+		rv = -SP.kron(self.getFstar()[i][:,[r]],self.getAstar()[i][[c],:])
+		return rv
+
+    #########################################
+    # Params manipulation 
+    #########################################
 
 	def getParams(self):
 		""" get params """
@@ -70,70 +198,37 @@ class mean():
 			self.B[i] = SP.reshape(params[start:start+np],self.B[i].shape, order='F')
 			start += np
 
+    #########################################
+    # Utility functions
+    #########################################
+
 	def getDimensions(self):
 		""" get phenotype dimensions """
 		return self.N,self.P
 
-	def setY(self,Y):
-		""" set phenotype """
-		self.N,self.P = Y.shape
-		self.Y = Y
-		self.Y_has_changed=True
+    def _set_toChange(x):
+        """ set variables in list x toChange """
+        for key in x.keys():
+            self.toChange[key] = True
 
-	def setRowRotation(self,Lr=None):
-		""" set row rotation """
-		if Lr is not None:
-			assert Lr.shape[0]==self.N, 'Lr dimension mismatch'
-			assert Lr.shape[1]==self.N, 'Lr dimension mismatch'
-		self.Lr = Lr
-		self.Lr_has_changed = True
-
-	def setColRotation(self,Lc=None):
-		""" set col rotation """
-		if Lc is not None:
-			assert Lc.shape[0]==self.P, 'Lc dimension mismatch'
-			assert Lc.shape[1]==self.P, 'Lc dimension mismatch'
-		self.Lc = Lc
-		self.Lc_has_changed = True
-
-	def getGradient(self,j):
-		""" get rotated gradient for fixed effect i """
-		self._rotate()
-		i = int(self.indicator['term'][j])
-		r = int(self.indicator['row'][j])
-		c = int(self.indicator['col'][j])
-		rv = -SP.kron(self.Fr[i][:,[r]],self.Ar[i][[c],:])
-		return rv
-
-	def _rotate(self):
-		""" rotate before rows using Lr and then rows using Lc """
-		if self.Lr_has_changed or self.Y_has_changed:
-			if self.Lr is not None:	self.Yr1 = SP.dot(self.Lr,self.Y)
-			else:					self.Yr1 = self.Y.copy()
-
-		if self.Lr_has_changed or self.designs_have_changed:
-			for term_i in range(self.n_terms):
-				if self.Lr is not None:	self.Fr[term_i] = SP.dot(self.Lr,self.F[term_i])
-				else:					self.Fr[term_i] = self.F[term_i].copy()
-
-		if self.Lc_has_changed or self.Y_has_changed:
-			if self.Lc is not None:	self.Yr = SP.dot(self.Yr1,self.Lc.T)
-			else:					self.Yr = self.Yr1.copy()
-
-		if self.Lc_has_changed or self.designs_have_changed:
-			for term_i in range(self.n_terms):
-				if self.Lc is not None:	self.Ar[term_i] = SP.dot(self.A[term_i],self.Lc.T)
-				else:					self.Ar[term_i] = self.A[term_i].copy()
-
-		"""
-		in principle here you can also set D and cache staff for DY 
-		not sure this is useful
-		"""
-
-		self.Lr_has_changed = False
-		self.Lc_has_changed = False
-		self.Y_has_changed  = False
-		self.designs_have_changed = False
+    def _init(self):
+        """ initializes all quantities """
+        # rotations and scalings
+        self.Lr = SP.eye(self.N)
+        self.Lc = SP.eye(self.P)
+        self.d  = SP.ones(self.N*self.P)
+        # pheno transformations
+        Ystar1 = Y.copy()
+        Ystar  = Y.copy()
+        Yhat   = Y.copy()
+        # fixed effect transformations
+        self.Astar = copy.copy(self.A)
+        self.Fstar = copy.copy(self.F)
+        # set toChange dict
+        x = ['Y','Ystar','Ystar1','Fstar','Astar','Xhat']
+        for key in x.keys():
+            self.toChange[key] = False
+        self.toChange['Xhat'] = True
 
 	def _update_indicator(self,K,L):
 		""" update the indicator """
