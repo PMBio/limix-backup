covariance update and bug fixes

limix/core/gp/gp_base.py
@@ -2,11 +2,10 @@ import pdb
 import scipy as SP
 import scipy.linalg as LA
 import copy
-
 import sys
 sys.path.append('./../../..')
-from mtSet.pycore.linalg.linalg_matrix import jitChol
-import mtSet.pycore.likelihood.likelihood_base
+from limix.core.linalg.linalg_matrix import jitChol
+import limix.core.likelihood.likelihood_base
 import scipy.lib.lapack.flapack
 
 

limix/core/gp/gp_base_new.py
@@ -2,18 +2,11 @@ import pdb
 import scipy as sp
 import scipy.linalg
 import copy
-
 import sys
 sys.path.insert(0,'./../../..')
 from limix.core.cobj import *
-#from limix.core.linalg.linalg_matrix import jitChol
 import limix.core.mean.mean_base
 import limix.core.covar.covariance
-import scipy.lib.lapack.flapack
-
-
-import logging as LG
-
 
 class gp(cObject):
     """
@@ -49,7 +42,8 @@ class gp(cObject):
                             'YKiY','YKiXB','LML',
                             'dKKiY','dKKiX','dKKiXB',
                             'YKiY_grad_i','YKiXB_grad_i','Areml_grad_i'
-                            'Areml_logdet_grad','YKiY_grad','YKiXB_grad','LML_grad')
+                            'Areml_logdet_grad','YKiY_grad',
+                            'YKiXB_grad','LML_grad')
         self.update_B()
 
     def getParams(self,params):

limix/core/tests/test_gp_base_new.py
@@ -6,59 +6,40 @@ from limix.core.covar.sqexp import sqexp
 from limix.core.gp.gp_base_new import gp
 
 import ipdb
-import scipy as SP
+import scipy as sp
 import scipy.linalg as LA 
 import time as TIME
 import copy
+import pylab as pl
+pl.ion()
 
 
 if __name__ == "__main__":
 
     # generate data
-    h2 = 0.3
-    N = 500; P = 3; S = 1000
-    X = 1.*(SP.rand(N,S)<0.2)
-    beta = SP.randn(S,P)
-    Yg = SP.dot(X,beta); Yg*=SP.sqrt(h2/Yg.var(0).mean())
-    Yn = SP.randn(N,P); Yn*=SP.sqrt((1-h2)/Yn.var(0).mean())
-    Y  = Yg+Yn; Y-=Y.mean(0); Y/=Y.std(0)
-    XX = SP.dot(X,X.T)
-    XX/= XX.diagonal().mean()
-    Xr = 1.*(SP.rand(N,10)<0.2)
-    Xr*= SP.sqrt(N/(Xr**2).sum())
+    N = 400
+    s_x = 0.05
+    s_y = 0.1
+    X = (sp.linspace(0,2,N)+s_x*sp.randn(N))[:,sp.newaxis]
+    Y = sp.sin(X)+s_y*sp.randn(N,1)
+    #pl.plot(x,y,'x')
 
     ipdb.set_trace()
 
     # define mean term
-    mu = mean(Y)
-    F = 1.*(SP.rand(N,2)<0.2)
-
-    # add second fixed effect
-    F2 = 1.*(SP.rand(N,3)<0.2); A2 = SP.ones((1,P))
-    
-    mu.addFixedEffect(F=F,A=A)
-    mu.addFixedEffect(F=F2,A=A2)
-
-    if mtSet_present:
-        mu1 = MEAN.mean(Y)
-        mu1.addFixedEffect(F=F,A=A)
-        mu1.addFixedEffect(F=F2,A=A2)
-    
+    F = 1.*(sp.rand(N,2)<0.2)
+    mu = lin_mean(Y,F)
 
     # define covariance matrices
-    Cg = freeform(P)
-    Cn = freeform(P)
-    Cg1 = freeform(P)
-    Cn1 = freeform(P)
+    covar = sqexp(X)
     
     ipdb.set_trace()
 
     if 1:
         # compare with mtSet implementation
         params = {}
-        params['Cg']   = SP.randn(int(0.5*P*(P+1)))
-        params['Cn']   = SP.randn(int(0.5*P*(P+1)))
-
+        params['Cg']   = sp.randn(int(0.5*P*(P+1)))
+        params['Cn']   = sp.randn(int(0.5*P*(P+1)))
 
         print "check gradient with gp2kronSum"
         gp = gp2kronSum(mu,Cg,Cn,XX)
@@ -74,7 +55,7 @@ if __name__ == "__main__":
         
         if mtSet_present:
             params1 = copy.copy(params)
-            params1['mean'] = SP.zeros(mu1.getParams().shape[0])
+            params1['mean'] = sp.zeros(mu1.getParams().shape[0])
             gp1 = gp2kronSumMtSet(mu1,Cg1,Cn1,XX)
             gp1.setParams(params1)
             start = TIME.time()
@@ -89,8 +70,8 @@ if __name__ == "__main__":
         # no fixed 
         mu = mean(Y)
         params = {}
-        params['Cg']   = SP.randn(int(0.5*P*(P+1)))
-        params['Cn']   = SP.randn(int(0.5*P*(P+1)))
+        params['Cg']   = sp.randn(int(0.5*P*(P+1)))
+        params['Cn']   = sp.randn(int(0.5*P*(P+1)))
 
         ipdb.set_trace()
         gp = gp2kronSum(mu,Cg,Cn,XX)