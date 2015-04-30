7f4983482f6c5c49499f8bfb68caefec87535674
paolo.casale <casale@ebi.ac.uk>
2015-05-01 00:52:52 +0100

    freeform added properties

--- limix/core/covar/freeform.py
+++ limix/core/covar/freeform.py
@@ -1,9 +1,11 @@
 import numpy as np
-import scipy as SP
+import scipy as sp
 import scipy.linalg as LA
 from covar_base import Covariance
 from limix.core.type.cached import cached
 import pdb
+
+import logging as LG
 
 class FreeFormCov(Covariance):
     """
@@ -13,19 +15,35 @@ class FreeFormCov(Covariance):
         Covariance.__init__(self, dim)
         self.n_params = int(dim * (dim + 1.) / 2.)
         self.dim = dim
-        self.params = SP.zeros(self.n_params)
+        self.params = sp.zeros(self.n_params)
 
-        self.L = SP.zeros((self.dim,self.dim))
-        self.Lgrad = SP.zeros((self.dim,self.dim))
-        self.zeros = SP.zeros(self.n_params)
+        self.L = sp.zeros((self.dim,self.dim))
+        self.Lgrad = sp.zeros((self.dim,self.dim))
+        self.zeros = sp.zeros(self.n_params)
         self.set_jitter(jitter)
 
     #####################
     # Properties
     #####################
     @property
-    def scale(self):
-        return sp.exp(self.params[0])
+    def variance(self):
+        return self.K().diagonal() 
+
+    @property
+    def correlation(self):
+        R = self.K()
+        inv_diag = 1./sp.sqrt(R.diagonal())[:,sp.newaxis]
+        R /= inv_diag
+        R /= inv_diag.T
+        return R
+
+    @property
+    def variance_ste(self):
+        LG.critical("Implement variance_ste")
+
+    @property
+    def correlation_ste(self):
+        LG.critical("Implement correlation_ste")
 
     #####################
     # Params handling
@@ -39,7 +57,7 @@ class FreeFormCov(Covariance):
     def setCovariance(self,cov):
         """ set hyperparameters from given covariance """
         chol = LA.cholesky(cov,lower=True)
-        params = chol[SP.tril_indices(self.dim)]
+        params = chol[sp.tril_indices(self.dim)]
         self.setParams(params)
 
     #####################
@@ -48,15 +66,27 @@ class FreeFormCov(Covariance):
     @cached
     def K(self):
         self._updateL()
-        RV = SP.dot(self.L,self.L.T)+self.jitter*SP.eye(self.dim)
+        RV = sp.dot(self.L,self.L.T)+self.jitter*sp.eye(self.dim)
         return RV
 
     @cached
     def K_grad_i(self,i):
         self._updateL()
         self._updateLgrad(i)
-        RV = SP.dot(self.L,self.Lgrad.T)+SP.dot(self.Lgrad,self.L.T)
+        RV = sp.dot(self.L,self.Lgrad.T)+sp.dot(self.Lgrad,self.L.T)
         return RV[..., np.newaxis]
+
+    ####################
+    # Interpretable Params
+    ####################
+    def getInterParams(self):
+        R1 = self.variance()
+        R2 = self.correlation()[sp.tril_indices(self.dim)] 
+        R = sp.concatenate([R1,R2])
+        return R
+
+    def K_grad_interParam_i(self,i):
+        LG.critical("implement K_grad_interParam_i")
 
     ######################
     # Private functions
@@ -66,14 +96,14 @@ class FreeFormCov(Covariance):
         """
         construct the cholesky factor from hyperparameters
         """
-        self.L[SP.tril_indices(self.dim)] = self.params
+        self.L[sp.tril_indices(self.dim)] = self.params
 
     def _updateLgrad(self,i):
         """
         construct the cholesky factor from hyperparameters
         """
         self.zeros[i] = 1
-        self.Lgrad[SP.tril_indices(self.dim)] = self.zeros
+        self.Lgrad[sp.tril_indices(self.dim)] = self.zeros
         self.zeros[i] = 0
 
 if __name__ == '__main__':
