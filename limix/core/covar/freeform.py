2f4bde65ebf589fc8d2f8dddeac728477031b3bd
paolo.casale <casale@ebi.ac.uk>
2015-05-01 00:43:25 +0100

    freeform covariance update

--- limix/core/covar/freeform.py
+++ limix/core/covar/freeform.py
@@ -1,7 +1,8 @@
 import numpy as np
 import scipy as SP
 import scipy.linalg as LA
-from covariance import Covariance
+from covar_base import Covariance
+from limix.core.type.cached import cached
 import pdb
 
 class FreeFormCov(Covariance):
@@ -9,9 +10,6 @@ class FreeFormCov(Covariance):
     freeform covariance function
     """
     def __init__(self,dim,jitter=1e-4):
-        """
-        initialization
-        """
         Covariance.__init__(self, dim)
         self.n_params = int(dim * (dim + 1.) / 2.)
         self.dim = dim
@@ -22,56 +20,47 @@ class FreeFormCov(Covariance):
         self.zeros = SP.zeros(self.n_params)
         self.set_jitter(jitter)
 
-    def setParams(self,params):
-        """
-        set hyperparameters
-        """
-        self.params = params
-        # this is to make the old implementation work
-        self.params_have_changed = True
+    #####################
+    # Properties
+    #####################
+    @property
+    def scale(self):
+        return sp.exp(self.params[0])
 
-    def getParams(self):
-        return self.params
-
-    def getNumberParams(self):
-        """
-        return the number of hyperparameters
-        """
-        return self.n_params
+    #####################
+    # Params handling
+    #####################
+    def _calcNumberParams(self):
+        self.n_params = int(0.5*self.dim*(self.dim+1))
 
     def set_jitter(self,value):
         self.jitter = value
 
     def setCovariance(self,cov):
-        """
-        set hyperparameters from given covariance
-        """
+        """ set hyperparameters from given covariance """
         chol = LA.cholesky(cov,lower=True)
         params = chol[SP.tril_indices(self.dim)]
         self.setParams(params)
 
+    #####################
+    # Cached
+    #####################
+    @cached
     def K(self):
-        """
-        evaluates the kernel for given hyperparameters theta
-        """
         self._updateL()
         RV = SP.dot(self.L,self.L.T)+self.jitter*SP.eye(self.dim)
         return RV
 
-    def Kgrad_param(self,i):
-        """
-        partial derivative with repspect to the i-th hyperparamter theta[i]
-        """
+    @cached
+    def K_grad_i(self,i):
         self._updateL()
         self._updateLgrad(i)
         RV = SP.dot(self.L,self.Lgrad.T)+SP.dot(self.Lgrad,self.L.T)
         return RV[..., np.newaxis]
 
-    def _calcNumberParams(self):
-        """
-        calculates the number of parameters
-        """
-        self.n_params = int(0.5*self.dim*(self.dim+1))
+    ######################
+    # Private functions
+    ######################
 
     def _updateL(self):
         """
@@ -89,6 +78,6 @@ class FreeFormCov(Covariance):
 
 if __name__ == '__main__':
     n = 2
-    cov = freeform(n)
+    cov = FreeFormCov(n)
     print cov.K()
-    print cov.Kgrad_param(0)
+    print cov.K_grad_i(0)
