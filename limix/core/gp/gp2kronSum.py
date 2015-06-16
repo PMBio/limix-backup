8642c20946ad0a02891a9be45443dbd98bae080a
paolo.casale <casale@ebi.ac.uk>
2015-06-16 11:15:22 +0100

    gp update to new caching system

--- limix/core/gp/gp2kronSum.py
+++ limix/core/gp/gp2kronSum.py
@@ -35,62 +35,55 @@ class GP2KronSum(GP):
         self.mean.register(self.designs_have_changed, 'designs')
 
     def col_cov_has_changed(self):
-        self.clear_cache('LrYLc', 'DLrYLc', 'ALc', 'LW', 'dLW',
-                            'WKiy', 'vei_dLWb', 'yKiy', 'yKiWb', 'Sr_DLrYLc_Ctilde',
-                            'Sr_vei_dLWb_Ctilde', 'yKiy_grad_i', 'yKiWb_grad_i')
+        self.clear_cache('col_cov')
         self.clear_all()
 
     def row_cov_has_changed(self):
-        self.clear_cache('LrY', 'LrYLc', 'DLrYLc', 'LrF', 'LW', 'dLW',
-                            'WKiy', 'vei_dLWb', 'yKiy', 'yKiWb', 'Sr_DLrYLc_Ctilde',
-                            'Sr_vei_dLWb_Ctilde', 'yKiy_grad_i', 'yKiWb_grad_i')
+        self.clear_cache('row_cov')
         self.clear_all()
 
     def pheno_has_changed(self):
-        self.clear_cache('LrY', 'LrYLc', 'DLrYLc',
-                            'WKiy', 'vei_dLWb', 'yKiy', 'yKiWb', 'Sr_DLrYLc_Ctilde',
-                            'Sr_vei_dLWb_Ctilde', 'yKiy_grad_i', 'yKiWb_grad_i')
+        self.clear_cache('pheno')
         self.clear_all()
 
     def designs_have_changed(self):
-        self.clear_cache('LrF', 'ALc', 'LW', 'dLW', 'WKiy', 'vei_dLWb',
-                            'yKiWb', 'Sr_vei_dLWb_Ctilde', 'yKiWb_grad_i')
+        self.clear_cache('designs')
         self.clear_all()
 
 
     ######################
     # Transformed phenotype
     ######################
-    @cached
+    @cached('row_cov', 'pheno')
     def LrY(self):
         return sp.dot(self.covar.Lr(), self.mean.Y)
 
-    @cached
+    @cached(['row_cov', 'col_cov', 'pheno'])
     def LrYLc(self):
         return sp.dot(self.LrY(), self.covar.Lc().T)
 
-    @cached
+    @cached(['row_cov', 'col_cov', 'pheno'])
     def DLrYLc(self):
         return self.covar.D() * self.LrYLc()
 
     ######################
     # Transformed fixed effects
     ######################
-    @cached
+    @cached(['row_cov', 'designs'])
     def LrF(self):
         R = []
         for ti in range(self.mean.n_terms):
             R.append(sp.dot(self.covar.Lr(), self.mean.F[ti]))
         return R
 
-    @cached
+    @cached(['col_cov', 'designs'])
     def ALc(self):
         R = []
         for ti in range(self.mean.n_terms):
             R.append(sp.dot(self.mean.A[ti], self.covar.Lc().T))
         return R
 
-    @cached
+    @cached(['row_cov', 'col_cov', 'designs'])
     def LW(self):
         R = sp.zeros((self.mean.Y.size, self.mean.n_covs))
         istart = 0
@@ -99,7 +92,7 @@ class GP2KronSum(GP):
             R[:, istart:iend] = sp.kron(self.ALc()[ti].T, self.LrF()[ti])
         return R
 
-    @cached
+    @cached(['row_cov', 'col_cov', 'designs'])
     def dLW(self):
         return self.covar.d()[:,sp.newaxis] * self.LW()
 
@@ -122,7 +115,7 @@ class GP2KronSum(GP):
     ########################
     # LML terms
     ########################
-    @cached
+    @cached(['row_cov', 'col_cov', 'designs', 'pheno'])
     def WKiy(self):
         R = sp.zeros((self.mean.n_covs, 1))
         istart = 0
@@ -136,38 +129,39 @@ class GP2KronSum(GP):
     def update_b(self):
         self.mean.b = self.Areml.solve(self.WKiy())
 
-    @cached
+    @cached(['row_cov', 'col_cov', 'designs', 'pheno'])
     def vei_dLWb(self):
         # could be optimized but probably not worth it
         # as it requires a for loop
         r = sp.dot(self.dLW(), self.mean.b)
         return r.reshape(self.mean.Y.shape, order = 'F')
 
-    @cached
+    @cached(['row_cov', 'col_cov', 'pheno'])
     def yKiy(self):
         return (self.LrYLc()*self.DLrYLc()).sum()
 
-    @cached
+    @cached(['row_cov', 'col_cov', 'designs', 'pheno'])
     def yKiWb(self):
         return (self.LrYLc() * self.vei_dLWb()).sum()
 
     #########################
     # Gradients
     #########################
-    @cached
+    @cached(['row_cov', 'col_cov', 'pheno'])
     def Sr_DLrYLc_Ctilde(self, i):
         return self.covar.Sr_X_Ctilde(self.DLrYLc(), i)
 
-    @cached
+    @cached(['row_cov', 'col_cov', 'designs', 'pheno'])
     def Sr_vei_dLWb_Ctilde(self, i):
         return self.covar.Sr_X_Ctilde(self.vei_dLWb(), i)
 
-    @cached
+    @cached(['row_cov', 'col_cov', 'pheno'])
     def yKiy_grad_i(self,i):
         return -(self.DLrYLc() * self.Sr_DLrYLc_Ctilde(i)).sum()
 
-    @cached
+    @cached(['row_cov', 'col_cov', 'designs', 'pheno'])
     def yKiWb_grad_i(self,i):
         rv = -2*(self.DLrYLc()*self.Sr_vei_dLWb_Ctilde(i)).sum()
         rv+= (self.vei_dLWb()*self.Sr_vei_dLWb_Ctilde(i)).sum()
         return rv
+
