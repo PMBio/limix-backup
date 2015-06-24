59ef344e4315a3269c0bee7277260ea6cc09abe4
paolo.casale <casale@ebi.ac.uk>
2015-06-24 10:03:33 +0100

    added -stdlib=libc++ flag

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ def get_swig_opts():
     return swig_opts
 
 def get_extra_compile_args():
-    return ['-std=c++0x']
+    return ['-std=c++0x', '--stdlib=libc++']
     #return []
 
 import numpy
