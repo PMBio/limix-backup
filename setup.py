7dffb92e1758c65f86df6b7775c8dc773b83fba8
Danilo Horta <danilo.horta@gmail.com>
2015-07-01 17:52:00 +0100

    travis wait

--- setup.py
+++ setup.py
@@ -151,7 +151,8 @@ def get_swig_opts():
 def get_extra_compile_args():
     return ['-Wno-comment', '-Wno-unused-but-set-variable',
             '-Wno-overloaded-virtual', '-Wno-uninitialized',
-            '-Wno-unused-const-variable', '-Wno-unknown-warning-option']
+            '-Wno-unused-const-variable', '-Wno-unknown-warning-option',
+            '-Wno-shorten-64-to-32']
 
 def try_to_add_compile_args():
     return ['-std=c++0x', '-stdlib=libc++']
