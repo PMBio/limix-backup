d469e1cf9addcc09b3d36ecb711ca57089cd2bd0
paolo.casale <casale@ebi.ac.uk>
2015-07-02 03:12:13 +0200

    minor bug fix

--- limix/utils/svd_utils.py
+++ limix/utils/svd_utils.py
@@ -1,13 +1,14 @@
 import numpy.linalg as nla
+import scipy as sp
 
 def svd_reduce(X, tol = 1e-9):
     U, Sh, V = nla.svd(X, full_matrices=0)
     I = Sh < tol
     if I.any():
         #warnings.warn('G has dependent columns, dimensionality reduced')
-        Sh = Sgh[~I]
-        U  = Ug[:, ~I]
-        V  = SP.eye(Sgh.shape[0])
-        X  = U * Sh[SP.newaxis,:]
+        Sh = Sh[~I]
+        U  = U[:, ~I]
+        V  = sp.eye(Sh.shape[0])
+        X  = U * Sh[sp.newaxis,:]
     S = Sh**2
     return X, U, S, V
