ca0fdce83f18e07b29bd064c67335d648b0bc07f
paolo.casale <casale@ebi.ac.uk>
2015-07-17 01:42:48 +0100

    added abstract combinator and kronCov in init

--- limix/core/covar/__init__.py
+++ limix/core/covar/__init__.py
@@ -3,6 +3,7 @@ from sqexp import SQExpCov
 from freeform import FreeFormCov
 from lowrank import LowRankCov
 from combinators import SumCov
+from kronecker import KronCov
 from cov2kronSum import Cov2KronSum
 from cov2kronSumLR import Cov2KronSumLR
 from cov3kronSumLR import Cov3KronSumLR
