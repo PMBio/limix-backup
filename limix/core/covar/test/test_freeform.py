fb47b0e08c397fbc94063a5ddc5e5f331d6f67d1
paolo.casale <casale@ebi.ac.uk>
2015-07-30 14:27:18 +0100

    added hessian for freeform

--- limix/core/covar/test/test_freeform.py
+++ limix/core/covar/test/test_freeform.py
@@ -42,5 +42,23 @@ class TestFreeForm(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.C.K_grad_i(0)
 
+    def test_Khess(self):
+
+        cov = self.C
+
+        for j in range(cov.getNumberParams()):
+
+            def func(x, i):
+                cov.setParams(x)
+                return cov.K_grad_i(j)
+
+            def grad(x, i):
+                cov.setParams(x)
+                return cov.K_hess_i_j(j, i)
+
+            x0 = cov.getParams()
+            err = mcheck_grad(func, grad, x0)
+            np.testing.assert_almost_equal(err, 0.)
+
 if __name__ == '__main__':
     unittest.main()
