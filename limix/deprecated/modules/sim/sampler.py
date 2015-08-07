524a29159a3dde59309a2ae21d79b2ccd16ca423
Danilo Horta <danilo.horta@gmail.com>
2015-08-07 09:48:55 +0100

    sim notes updated, saving cov

--- limix/deprecated/modules/sim/sampler.py
+++ limix/deprecated/modules/sim/sampler.py
@@ -136,11 +136,12 @@ if __name__ == '__main__':
     from dreader import normalize_genotype
     import h5py
     ncovariates = 1
-    # nindividuals = 500
+    # nindividuals = 1000
     # nparents = 2
     #
-    # fore_chroms = [22]
+    # fore_chroms = [20]
     # back_chroms = [21]
+    # back2_chroms = [22]
     # pops = ['EUR']
     # maf = 0.05
     #
@@ -150,40 +151,54 @@ if __name__ == '__main__':
     #
     #     fore_mean_std = dr.mean_std(fore_chroms)
     #     back_mean_std = dr.mean_std(back_chroms)
+    #     back2_mean_std = dr.mean_std(back2_chroms)
     #
     #     fore_genos = []
     #     back_genos = []
+    #     back2_genos = []
     #     covariatess = []
     #     for i in xrange(nindividuals):
     #         parents = dr.choose_parents(nparents)
     #         covariate = np.random.randn(ncovariates)
     #         fore_geno = dr.generate_genotype(parents, fore_chroms)
     #         back_geno = dr.generate_genotype(parents, back_chroms)
+    #         back2_geno = dr.generate_genotype(parents, back2_chroms)
     #
     #         covariatess.append(covariate)
     #         fore_genos.append(fore_geno)
     #         back_genos.append(back_geno)
+    #         back2_genos.append(back2_geno)
     #
     #     fG = np.array(fore_genos, float)
     #     bG = np.array(back_genos, float)
+    #     b2G = np.array(back2_genos, float)
+    #     nb2G = standardize_genotype(b2G, back2_mean_std)
+    #     b2K = dot(nb2G, nb2G.T) / nb2G.shape[1]
+    #
+    #
     #     X = np.array(covariatess, float)
     #
-    #     with h5py.File("/Users/horta/workspace/sample500.hdf5", "w") as f:
+    #     def create_dataset(grp, name, M):
+    #         grp.create_dataset(name, chunks=(1, M.shape[1]),
+    #                            compression='lzf', data=M)
+    #
+    #     with h5py.File("/Users/horta/workspace/sample1000.hdf5", "w") as f:
     #         ggrp = f.create_group("genotypes")
-    #         ggrp.create_dataset("fG_inds_by_snps", chunks=(1, fG.shape[1]),
-    #                             compression='lzf', data=fG)
-    #         ggrp.create_dataset("fG_snps_by_inds", chunks=(1, fG.shape[1]),
-    #                             compression='lzf', data=fG)
+    #
+    #         create_dataset(ggrp, "fG_inds_by_snps", fG)
+    #         create_dataset(ggrp, "fG_snps_by_inds", fG.T)
     #         ggrp.create_dataset("fore_mean_std", data=np.asarray(fore_mean_std))
-    #         ggrp.create_dataset("bG_inds_by_snps", chunks=(1, bG.shape[1]),
-    #                             compression='lzf', data=bG)
-    #         ggrp.create_dataset("bG_snps_by_inds", chunks=(1, bG.shape[1]),
-    #                             compression='lzf', data=bG)
+    #
+    #         create_dataset(ggrp, "bG_inds_by_snps", bG)
+    #         create_dataset(ggrp, "bG_snps_by_inds", bG.T)
     #         ggrp.create_dataset("back_mean_std", data=np.asarray(back_mean_std))
+    #
+    #         ggrp.create_dataset("b2K", data=b2K)
+    #
     #         cgrp = f.create_group("covariates")
     #         cgrp.create_dataset("X", data=X)
 
-    with h5py.File("/Users/horta/workspace/sample500.hdf5", "r") as f:
+    with h5py.File("/Users/horta/workspace/sample1000.hdf5", "r") as f:
         fore_var = 0.4
         back_var = 0.4
         back2_var = 1.0
@@ -205,8 +220,7 @@ if __name__ == '__main__':
         ns.add_effects_design("foreground", binary_effsiz_sampler(fore_var), fG)
         ns.add_effects_design("background", normal_effsiz_sampler(back_var), bG)
 
-        K = dot(bG, bG.T) / bG.shape[1]
-        K = standardize_covariance(K)
+        K = genos['b2K'][:]
         ns.add_effect_cov("background2", K, back2_var)
 
         ns.set_noise(noise_var)
