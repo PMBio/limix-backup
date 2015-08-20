248d3a13d3f154ae56ba9bcb6caebd81885cd5db
Danilo Horta <danilo.horta@gmail.com>
2015-08-20 11:11:34 +0100

    version working

--- limix/_version.py
+++ limix/_version.py
@@ -36,7 +36,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "limix-"
+    cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "limix/_version.py"
     cfg.verbose = False
