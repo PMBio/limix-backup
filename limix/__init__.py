9e4158e762bf23e329e328c3caf1beb05601113e
Danilo Horta <danilo.horta@gmail.com>
2016-03-31 00:06:50 +0100

    main init proper importing

--- limix/__init__.py
+++ limix/__init__.py
@@ -1,6 +1,6 @@
-import limix
-from mtSet import MTSet
-
+from __future__ import absolute_import
+from .mtSet import MTSet
+from .version import version as __version__
 
 def test():
 	from unittest import TestLoader
@@ -9,7 +9,3 @@ def test():
 	folder = os.path.dirname(os.path.realpath(__file__))
 	suite = TestLoader().discover(folder)
 	TextTestRunner(verbosity=2).run(suite)
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
