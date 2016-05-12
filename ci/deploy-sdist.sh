f594b9367b4281eb01a0eb80cf95b32a37c2757a
Danilo Horta <danilo.horta@gmail.com>
2016-05-13 00:39:07 +0100

    concourse params

--- ci/deploy-sdist.sh
+++ ci/deploy-sdist.sh
@@ -2,5 +2,19 @@
 
 set -e -x
 
+PYPI_USERNAME=$1
+PYPI_PASSWORD=$2
+
+VERSION=`cat limix/setup.py | grep 'VERSION *= *' | sed "s/VERSION *= *'\(.*\)'/\1/g"`
+
+echo $VERSION | grep dev > /dev/null
+status_code=$?
+if [ status_code -ne 0 ]
+then
+    exit 0
+fi
+
 FILENAME=`ls limix-sdist/ | head -1`
 echo $FILENAME
+cmd="twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD limix-sdist/$FILENAME"
+for i in {1..5}; do eval $cmd && break || sleep 15; done
