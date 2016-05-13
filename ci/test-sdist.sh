e472f39809e5f05d0c284d9ce21d5b198e10a7da
Danilo Horta <danilo.horta@gmail.com>
2016-05-13 22:36:42 +0100

    sdist

--- ci/test-sdist.sh
+++ ci/test-sdist.sh
@@ -3,7 +3,6 @@
 set -e -x
 
 ls
-# FILENAME=`ls limix-sdist/ | head -1`
-# pip install limix-sdist/$FILENAME
-# python -c "import limix; print(limix.__version__)"
-find .
+FILENAME=`ls sdist/ | head -1`
+pip install sdist/$FILENAME
+python -c "import limix; print(limix.__version__)"
